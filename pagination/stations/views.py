c78c8b1e57d5ccac350f324c070645d73675b050
Muromets812 <63260729+Muromets812@users.noreply.github.com>
2023-05-22 23:21:28 +0300

    work version

--- pagination/stations/views.py
+++ pagination/stations/views.py
@@ -9,16 +9,14 @@ def index(request):
 
 
 def bus_stations(request):
-    page_number = int(request.GET.get('page', 1))
-    with open('data-398-2018-08-30.csv', newline='', encoding='UTF-8') as csvfile:
-        reader = csv.DictReader(csvfile)
+    with open('data-398-2018-08-30.csv', newline='', encoding='UTF-8') as f:
+        page_number = int(request.GET.get('page', 1))
         bus_stations_list = []
-        for row in reader:
-            bus_stations_list.append(row)
-    paginator = Paginator(bus_stations_list, 10)
-    page = paginator.get_page(page_number)
-    context = {
-         'bus_stations': bus_stations_list,
-         'page': page,
-    }
+        for row in csv.DictReader(f): bus_stations_list.append(row)
+        paginator = Paginator(bus_stations_list, 10)
+        page = paginator.get_page(page_number)
+        context = {
+             'bus_stations': page,
+             'page': page,
+        }
     return render(request, 'stations/index.html', context)
